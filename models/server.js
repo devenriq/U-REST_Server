1f427f7ab315cfac072dc736b119ddaa1b03b1b5
enrique palomino <octepc.97@gmail.com>
2023-09-30 13:58:18 -0500

    feat: :art: Added the CRUD logic

--- models/server.js
+++ models/server.js
@@ -19,7 +19,34 @@ class Server {
 
   routes() {
     this.app.get("/api", (req, res) => {
-      res.send("Hello World");
+      res.json({
+        ok: true,
+        msg: "get API",
+      });
+    });
+    this.app.put("/api", (req, res) => {
+      res.json({
+        ok: true,
+        msg: "put API",
+      });
+    });
+    this.app.post("/api", (req, res) => {
+      res.status(201).json({
+        ok: true,
+        msg: "post API",
+      });
+    });
+    this.app.delete("/api", (req, res) => {
+      res.json({
+        ok: true,
+        msg: "delete API",
+      });
+    });
+    this.app.patch("/api", (req, res) => {
+      res.json({
+        ok: true,
+        msg: "patch API",
+      });
     });
   }
 
